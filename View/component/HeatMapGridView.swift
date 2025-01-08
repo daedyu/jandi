eb08ae756727cb71fcaff188762f6ed6ee6be2a4
kmg <legolove08@naver.com>
2025-01-08 21:36:31 +0900

    refactor: heatmap

--- View/component/HeatMapGridView.swift
+++ View/component/HeatMapGridView.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 struct HeatmapGridView: View {
     let data: [Date: Int]
+    let year: Int
     @Binding var selectedDate: Date?
     @Binding var isSelected: Bool
     
@@ -31,26 +32,69 @@ struct HeatmapGridView: View {
                     )
             }
         }
-        .padding(.horizontal)
+            .padding(.horizontal, 3)
     }
     
     private func getDatesGrid() -> [Date] {
         let calendar = Calendar.current
-        let endDate = Date()
-        let startDate = calendar.date(byAdding: .year, value: -1, to: endDate)!
+        let today = Date()
+        
+        var dateComponents = DateComponents()
+        dateComponents.year = year
+        dateComponents.month = 1
+        dateComponents.day = 1
+        let startDate = calendar.date(from: dateComponents)!
+        
+        dateComponents.year = year
+        dateComponents.month = 12
+        dateComponents.day = 31
+        let yearEndDate = calendar.date(from: dateComponents)!
+        
+        let endDate = min(today, yearEndDate)
         
         var dates: [Date] = []
         var currentDate = startDate
         
-        while currentDate <= endDate {
-            dates.append(currentDate)
+        while calendar.component(.weekday, from: currentDate) != 1 {
+            currentDate = calendar.date(byAdding: .day, value: -1, to: currentDate)!
+        }
+        
+        var adjustedEndDate = endDate
+        while calendar.component(.weekday, from: adjustedEndDate) != 7 {
+            adjustedEndDate = calendar.date(byAdding: .day, value: 1, to: adjustedEndDate)!
+        }
+        
+        while currentDate <= adjustedEndDate {
+            if currentDate <= today {
+                dates.append(currentDate)
+            }
             currentDate = calendar.date(byAdding: .day, value: 1, to: currentDate)!
         }
         
-        return dates
+        let weeks = stride(from: 0, to: dates.count, by: 7).map {
+            Array(dates[($0)..<min($0 + 7, dates.count)])
+        }
+        
+        var sortedDates: [Date] = []
+        for column in 0..<weeks.count {
+            for row in 0..<7 {
+                if column < weeks.count && row < weeks[column].count {
+                    sortedDates.append(weeks[column][row])
+                }
+            }
+        }
+        
+        return sortedDates
     }
     
     private func colorForDate(_ date: Date) -> Color {
+        let calendar = Calendar.current
+        let selectedYear = calendar.component(.year, from: date)
+        
+        if selectedYear < year || selectedYear > year {
+            return Color(.systemGray2)
+        }
+        
         let count = data[Calendar.current.startOfDay(for: date)] ?? 0
         
         switch count {
